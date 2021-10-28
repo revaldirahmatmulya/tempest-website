1ca58b5f3d53687ce21f634b3aab066b26d19623
Revaldi Rahmatmulya <revaldiaye@gmail.com>
2021-10-28 22:32:34 +0700

    fix responsive

--- vendor/scroll.js
+++ vendor/scroll.js
@@ -1,15 +1,17 @@
-$('.scroll').on('click',function (e) {
-	
-	//ambil isi href
-	var tujuan = $(this).attr('href');
-	//tangkap element
-	var elemenTujuan = $(tujuan);
-	
-	//pindahkan scroll
-	$('html,body').animate({
-		scrollTop: elemenTujuan.offset().top -50
-	},300,'swing');
+$(".scroll").on("click", function (e) {
+  //ambil isi href
+  var tujuan = $(this).attr("href");
+  //tangkap element
+  var elemenTujuan = $(tujuan);
 
-	e.preventDefault();
-	
+  //pindahkan scroll
+  $("html,body").animate(
+    {
+      scrollTop: elemenTujuan.offset().top,
+    },
+    300,
+    "swing"
+  );
+
+  e.preventDefault();
 });
